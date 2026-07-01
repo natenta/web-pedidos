e7d4e3545d03103563758e46149515fe31fe8ff9
juanoxidev <123821231+juanoxidev@users.noreply.github.com>
2026-06-30 22:59:22 -0300

    Delete src directory

--- src/models/Topping.ts
+++
@@ -1,9 +0,0 @@
-import type { ToppingCategory } from '../types';
-
-export class Topping {
-  constructor(
-    public readonly id: string,
-    public readonly name: string,
-    public readonly category: ToppingCategory
-  ) {}
-}
